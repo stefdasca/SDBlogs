7e2467be94efee8830cadd8f30e38c24e5c9962b
stefdasca <cosmin_stefan_d@yahoo.com>
2025-05-13 19:23:43 +0200

    bad repo thing

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 module.exports = {
-  // static export
   output: 'export',
-  // PROJECT-PAGE path (must match your repo name exactly)
-  basePath: '/SDBlogs',
-  assetPrefix: '/SDBlogs/',
   trailingSlash: true,
 }
