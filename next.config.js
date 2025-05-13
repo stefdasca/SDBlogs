db5cca04c7a73e28cd7ae36f259c22f9dd9ab77f
stefdasca <cosmin_stefan_d@yahoo.com>
2025-05-13 20:44:38 +0200

    reset to new format

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-/** next.config.js **/
+// next.config.js
 /** @type {import('next').NextConfig} */
 module.exports = {
-  // Classic pages-router export mode
-  output: 'export',
-  trailingSlash: true,
+  trailingSlash: true, // optional
 }
